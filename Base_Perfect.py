48a28942d88b4fa54a9a1bb29cd34ec800b6b878
yahyash61 <yahyasharifi1361@gmail.com>
2026-06-25 00:05:17 +0330

    deployment script for HelloBase contract

---
+++ Base_Perfect.py
@@ -0,0 +1,14 @@
+const hre = require("hardhat");
+
+async function main() {
+  const HelloBase = await hre.ethers.getContractFactory("HelloBase");
+  const helloBase = await HelloBase.deploy();
+
+  await helloBase.waitForDeployment();
+  console.log(`HelloBase deployed to: ${await helloBase.getAddress()}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
